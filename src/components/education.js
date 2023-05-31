8bb46b19e28fb7fe206a620e8fb591634e33fa2f
unbearabletruth <antaarty@gmail.com>
2023-05-31 16:31:42 +0700

    other components to funtion

--- src/components/education.js
+++ src/components/education.js
@@ -1,79 +1,73 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 import '../styles/education.css'; 
 
-export default class Education extends Component{
-    constructor(props){
-        super(props)
+export default function Education({editable}){
+    const [data, setData] = useState({
+        school: "",
+        title: "",
+        date: "",
+    })
 
-        this.state = {
-            school: "",
-            title: "",
-            date: "",
-        }
-    }
-
-    handleChange = (e) => {
-        this.setState({
+    const handleChange = (e) => {
+        setData({
+            ...data,
             [e.target.name] : e.target.value
         }) 
     }
 
-    render(){
-        let {editable} = this.props;
-        return (
-            <div id="educationWrapper">
-                {editable ?
-                    <>
-                        <div id="educationForm">
-                        <h2>Fill in education info
-                            <hr></hr>
-                        </h2>
-                            <div className='propertyEd'>
-                                <label>School name:</label>
-                                <input 
-                                    name = "school" 
-                                    value={this.state.school} 
-                                    onChange={this.handleChange}>      
-                                </input>
-                            </div> 
-                            <div className='propertyEd'>
-                                <label>Title of study:</label>
-                                <input 
-                                    name = "title" 
-                                    value={this.state.title} 
-                                    onChange={this.handleChange}>
-                                </input>
-                            </div>
-                            <div className='propertyEd'>
-                                <label>Date of study:</label>
-                                <input 
-                                    name = "date" 
-                                    value={this.state.date} 
-                                    onChange={this.handleChange}>
-                                </input>
-                            </div> 
+    return (
+        <div id="educationWrapper">
+            {editable ?
+                <>
+                    <div id="educationForm">
+                    <h2>Fill in education info
+                        <hr></hr>
+                    </h2>
+                        <div className='propertyEd'>
+                            <label>School name:</label>
+                            <input 
+                                name = "school" 
+                                value={data.school} 
+                                onChange={handleChange}>      
+                            </input>
                         </div> 
-                    </>
-                    :
-                    <div id="education">
-                        <h2>Education
-                            <hr></hr>
-                        </h2>
                         <div className='propertyEd'>
-                            <p className='titleTextEd'>School</p>
-                            <p className='infoTextEd'>{this.state.school}</p>
+                            <label>Title of study:</label>
+                            <input 
+                                name = "title" 
+                                value={data.title} 
+                                onChange={handleChange}>
+                            </input>
                         </div>
                         <div className='propertyEd'>
-                            <p className='titleTextEd'>Study</p>
-                            <p className='infoTextEd'>{this.state.title}</p>
-                        </div>
-                        <div className='propertyEd'>
-                            <p className='titleTextEd'>Date of study</p>
-                            <p className='infoTextEd'>{this.state.date}</p>
-                        </div>
-                    </div>      
-                }    
-            </div>
-        )
-    }
+                            <label>Date of study:</label>
+                            <input 
+                                name = "date" 
+                                value={data.date} 
+                                onChange={handleChange}>
+                            </input>
+                        </div> 
+                    </div> 
+                </>
+                :
+                <div id="education">
+                    <h2>Education
+                        <hr></hr>
+                    </h2>
+                    <div className='propertyEd'>
+                        <p className='titleTextEd'>School</p>
+                        <p className='infoTextEd'>{data.school}</p>
+                    </div>
+                    <div className='propertyEd'>
+                        <p className='titleTextEd'>Study</p>
+                        <p className='infoTextEd'>{data.title}</p>
+                    </div>
+                    <div className='propertyEd'>
+                        <p className='titleTextEd'>Date of study</p>
+                        <p className='infoTextEd'>{data.date}</p>
+                    </div>
+                </div>      
+            }    
+        </div>
+    )
 }
--- src/components/practicalExperience.js
+++ src/components/practicalExperience.js
@@ -1,33 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 import '../styles/experience.css';
 import uniqid from "uniqid";
 
-export default class PracticalExperience extends Component{
-    constructor(props){
-        super(props)
+export default function PracticalExperience({editable}){
+    const [data, setData] = useState({
+        exp: {
+            company: "",
+            position: "",
+            tasks: "",
+            date: "",
+            id: uniqid()
+        },
+        allExps: [{
+            company: "",
+            position: "",
+            tasks: "",
+            date: "",
+            id: uniqid()
+        }],
+        tooManyExp: false
+    }) 
 
-        this.state = {
-            exp: {
-                company: "",
-                position: "",
-                tasks: "",
-                date: "",
-                id: uniqid()
-            },
-            allExps: [{
-                company: "",
-                position: "",
-                tasks: "",
-                date: "",
-                id: uniqid()
-            }],
-            tooManyExp: false
-        }
-    }
-
-    handleChange = (e, id) => {
-        this.setState({
-            allExps: this.state.allExps.map((exp) => {
+    const handleChange = (e, id) => {
+        setData({
+            allExps: data.allExps.map((exp) => {
                 if(exp.id === id){
                     exp[e.target.name] = e.target.value
                     return exp;
@@ -37,16 +33,16 @@ export default class PracticalExperience extends Component{
         }) 
     }
 
-    addExp = (e) => {
+    const addExp = (e) => {
         e.preventDefault();
-        if(this.state.allExps.length === 2){
-            this.setState({
+        if(data.allExps.length === 2){
+            setData({
                 tooManyExp: true
             })
             return
         }
-        this.setState({
-          allExps: this.state.allExps.concat(this.state.exp),
+        setData({
+          allExps: data.allExps.concat(data.exp),
           exp: {
             company: "",
             position: "",
@@ -56,87 +52,85 @@ export default class PracticalExperience extends Component{
         },
         });
       }
-
-    render(){
-        let {editable} = this.props;
-        return (
-            <div id="experienceWrapper">
-                {this.state.allExps.map((exp) => {
-                    if(editable){
-                        return(
-                            <div key={exp.id} id="experienceForm">
-                                <h2>Fill in experience info
-                                    <hr className='expHr'></hr>
-                                </h2>
-                                <div className='propertyExp'>
-                                    <label>Company name</label>
-                                    <input 
-                                        className="expInput" 
-                                        name = "company" 
-                                        value={exp.company} 
-                                        onChange={(e) => this.handleChange(e, exp.id)}>
-                                    </input>
-                                </div>
-                                <div className='propertyExp'>
-                                    <label>Positon title</label>
-                                    <input 
-                                        className="expInput" 
-                                        name = "position" 
-                                        value={exp.position} 
-                                        onChange={(e) => this.handleChange(e, exp.id)}>
-                                    </input>     
-                                </div>
-                                <div className='propertyExp'>
-                                    <label>Main tasks</label>
-                                    <textarea 
-                                        className="expInput" 
-                                        name = "tasks" 
-                                        value={exp.tasks} 
-                                        onChange={(e) => this.handleChange(e, exp.id)}>
-                                    </textarea>
-                                </div>
-                                <div className='propertyExp'>
-                                    <label>Date from and until you worked for the company</label>
-                                    <input 
-                                        className="expInput" 
-                                        name = "date" 
-                                        value={exp.date} 
-                                        onChange={(e) => this.handleChange(e, exp.id)}>
-                                    </input>
-                                </div>  
-                            </div> 
-                        )  
-                    }
-                    else {
-                        console.log(exp)
-                        return(
-                        <div key={exp.id} id="experience">
-                            <h2>{`Job position at ${exp.company}`}
+      console.log(data.allExps)
+    return (
+        <div id="experienceWrapper">
+            {data.allExps.map((exp) => {
+                console.log(exp)
+                if(editable){
+                    return(
+                        <div key={exp.id} id="experienceForm">
+                            <h2>Fill in experience info
                                 <hr className='expHr'></hr>
                             </h2>
                             <div className='propertyExp'>
-                                <p className='titleTextExp'>Company</p>
-                                <p className='infoTextExp'>{exp.company}</p>
+                                <label>Company name</label>
+                                <input 
+                                    className="expInput" 
+                                    name = "company" 
+                                    value={exp.company} 
+                                    onChange={(e) => handleChange(e, exp.id)}>
+                                </input>
                             </div>
                             <div className='propertyExp'>
-                                <p className='titleTextExp'>Position</p>
-                                <p className='infoTextExp'>{exp.position}</p>
+                                <label>Positon title</label>
+                                <input 
+                                    className="expInput" 
+                                    name = "position" 
+                                    value={exp.position} 
+                                    onChange={(e) => handleChange(e, exp.id)}>
+                                </input>     
                             </div>
                             <div className='propertyExp'>
-                                <p className='titleTextExp'>Main tasks</p>
-                                <p className='infoTextExp'>{exp.tasks}</p>
+                                <label>Main tasks</label>
+                                <textarea 
+                                    className="expInput" 
+                                    name = "tasks" 
+                                    value={exp.tasks} 
+                                    onChange={(e) => handleChange(e, exp.id)}>
+                                </textarea>
                             </div>
                             <div className='propertyExp'>
-                                <p className='titleTextExp'>From and Until</p>
-                                <p className='infoTextExp'>{exp.date}</p>
-                            </div>
-                        </div>   
-                        )
-                    }    
-                })}
-                {editable ? <button id="addExp" onClick={this.addExp}>Add another job</button> : null}
-                {editable && this.state.tooManyExp ? <p id="tooManyExp">You can only add 2 jobs</p> : null}    
-            </div>
-        )
-    }
+                                <label>Date from and until you worked for the company</label>
+                                <input 
+                                    className="expInput" 
+                                    name = "date" 
+                                    value={exp.date} 
+                                    onChange={(e) => handleChange(e, exp.id)}>
+                                </input>
+                            </div>  
+                        </div> 
+                    )  
+                }
+                else {
+                    return(
+                    <div key={exp.id} id="experience">
+                        <h2>{`Job position at ${exp.company}`}
+                            <hr className='expHr'></hr>
+                        </h2>
+                        <div className='propertyExp'>
+                            <p className='titleTextExp'>Company</p>
+                            <p className='infoTextExp'>{exp.company}</p>
+                        </div>
+                        <div className='propertyExp'>
+                            <p className='titleTextExp'>Position</p>
+                            <p className='infoTextExp'>{exp.position}</p>
+                        </div>
+                        <div className='propertyExp'>
+                            <p className='titleTextExp'>Main tasks</p>
+                            <p className='infoTextExp'>{exp.tasks}</p>
+                        </div>
+                        <div className='propertyExp'>
+                            <p className='titleTextExp'>From and Until</p>
+                            <p className='infoTextExp'>{exp.date}</p>
+                        </div>
+                    </div>   
+                    )
+                }    
+            })}
+            {editable ? <button id="addExp" onClick={addExp}>Add another job</button> : null}
+            {editable && data.tooManyExp ? <p id="tooManyExp">You can only add 2 jobs</p> : null}    
+        </div>
+    )
 }
+
